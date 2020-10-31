b736d58763237fda092318cfba2204f0a3fbf652
DonggyuLee92 <60080220+DonggyuLee92@users.noreply.github.com>
2020-10-31 22:25:55 +0900

    1031

---
+++ 2020 Oct/1031/6249/6249.py
@@ -0,0 +1,53 @@
+import sys, math
+sys.stdin = open("6249.txt", "r")
+
+a = int(input())
+# b = 110%10
+ans_0 = 0
+ans_1 = 0
+ans_2 = 0
+ans_3 = 0
+ans_4 = 0
+ans_5 = 0
+ans_6 = 0
+ans_7 = 0
+ans_8 = 0
+ans_9 = 0
+while(a):
+    if(a%10 == 0):
+        ans_0 = ans_0 + 1
+        a = math.floor(a/10)
+    elif(a%10 == 1):
+        ans_1 = ans_1 + 1
+        a = math.floor(a/10)
+    elif(a%10 == 2):
+        ans_2 = ans_2 + 1
+        a = math.floor(a/10)
+    elif (a % 10 == 3):
+        ans_3 = ans_3 + 1
+        a = math.floor(a/10)
+    elif (a % 10 == 4):
+        ans_4 = ans_4 + 1
+        a = math.floor(a/10)
+    elif (a % 10 == 5):
+        ans_5 = ans_5 + 1
+        a = math.floor(a/10)
+    elif (a % 10 == 6):
+        ans_6 = ans_6 + 1
+        a = math.floor(a/10)
+    elif (a % 10 == 7):
+        ans_7 = ans_7 + 1
+        a = math.floor(a/10)
+    elif (a % 10 == 8):
+        ans_8 = ans_8 + 1
+        a = math.floor(a/10)
+    else:
+        ans_9 = ans_9 + 1
+        a = math.floor(a/10)
+
+    if(a < 1):
+        break
+
+print("0 1 2 3 4 5 6 7 8 9")
+print("{} {} {} {} {} {} {} {} {} {}".format(ans_0, ans_1, ans_2, ans_3, ans_4, ans_5, ans_6, ans_7, ans_8, ans_9))
+# print(b)
