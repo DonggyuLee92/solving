c726650d4c0b507cfa62767f4c662db895202c86
skdewww <skdewww@gmail.com>
2020-11-12 22:55:21 +0900

    1112

---
+++ 2020 Nov/1112/6308/6308.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("6308.txt","r", encoding="UTF8")
+from datetime import datetime, date
+
+name = input()
+age = int(input())
+now = date(2018, 1, 1).year
+
+print("{}(은)는 {}년에 100세가 될 것입니다.".format(name, now-age+101))
