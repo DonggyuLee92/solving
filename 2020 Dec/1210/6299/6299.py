850afee350180252b9cdfa2c316282410916c455
skdewww <skdewww@gmail.com>
2020-12-10 19:53:44 +0900

    1210

---
+++ 2020 Dec/1210/6299/6299.py
@@ -0,0 +1,10 @@
+input_list = [5, 6, 77, 45, 22, 12, 24]
+# ans = []
+# for i in input_list:
+#     if i%2 !=0:
+#         ans.append(i)
+# # ans = [if x for x%2!=0 in input_list]
+# print(ans)
+
+# ans = [x for x in input_list if x%2!=0]
+print([x for x in input_list if x%2!=0])
